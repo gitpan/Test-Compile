2d01ecf33fa3265fc0b6cceabb360442a6747383
Marcel Grünauer <marcel@cpan.org>
2009-10-08 20:40:35 +0000

    Import of MARCEL/Test-Compile-0.10 from CPAN.
    gitpan-cpan-distribution: Test-Compile
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         MARCEL/Test-Compile-0.10.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- inc/Module/Install/ReadmeMarkdownFromPod.pm
+++ inc/Module/Install/ReadmeMarkdownFromPod.pm
@@ -4,7 +4,6 @@ package Module::Install::ReadmeMarkdownFromPod;
 use 5.006;
 use strict;
 use warnings;
-use Pod::Markdown;
 
 our $VERSION = '0.02';
 
@@ -14,6 +13,10 @@ sub readme_markdown_from {
     my ($self, $file, $clean) = @_;
     return unless $Module::Install::AUTHOR;
     die "syntax: readme_markdown_from $file, [$clean]\n" unless $file;
+
+    # require, not use because otherwise Makefile.PL will complain if
+    # non-authors don't have Pod::Markdown, which would be bad.
+    require Pod::Markdown;
 
     my $parser = Pod::Markdown->new;
     $parser->parse_from_file($file);
@@ -43,4 +46,4 @@ sub reference_module {
 
 __END__
 
-#line 146
+#line 151
