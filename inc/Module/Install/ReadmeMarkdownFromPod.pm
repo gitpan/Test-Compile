f930536dd161501b588172c9f6df4c5e49b6fc0b
Marcel Grünauer <marcel@cpan.org>
2009-12-08 11:22:48 +0000

    Import of MARCEL/Test-Compile-0.11 from CPAN.
    gitpan-cpan-distribution: Test-Compile
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         MARCEL/Test-Compile-0.11.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- inc/Module/Install/ReadmeMarkdownFromPod.pm
+++ inc/Module/Install/ReadmeMarkdownFromPod.pm
@@ -5,7 +5,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use base qw(Module::Install::Base);
 
@@ -17,6 +17,7 @@ sub readme_markdown_from {
     # require, not use because otherwise Makefile.PL will complain if
     # non-authors don't have Pod::Markdown, which would be bad.
     require Pod::Markdown;
+    $self->admin->copy_package('Pod::Markdown', $INC{'Pod/Markdown.pm'});
 
     my $parser = Pod::Markdown->new;
     $parser->parse_from_file($file);
@@ -34,6 +35,24 @@ END
     1;
 }
 
+sub readme_markdown_from_pod {
+    my ($self, $clean) = @_;
+    return unless $Module::Install::AUTHOR;
+    unless ($self->Meta->{values}{all_from}) {
+        die "set 'all_from' or use 'readme_markdown_from'\n";
+    }
+    $self->readme_markdown_from($self->Meta->{values}{all_from}, $clean);
+}
+
+sub readme_from_pod {
+    my ($self, $clean) = @_;
+    return unless $Module::Install::AUTHOR;
+    unless ($self->Meta->{values}{all_from}) {
+        die "set 'all_from' or use 'readme_from'\n";
+    }
+    $self->readme_from($self->Meta->{values}{all_from}, $clean);
+}
+
 sub reference_module {
     my ($self, $file) = @_;
     die "syntax: reference_module $file\n" unless $file;
@@ -46,4 +65,4 @@ sub reference_module {
 
 __END__
 
-#line 151
+#line 188
